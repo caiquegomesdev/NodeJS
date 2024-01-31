df7f92a9a305f6f1224aafc40373e48f857e6c90
Caique <caiquegomesdev@gmail.com>
2024-01-30 22:20:46 -0300

    Projeto

--- 003-projeto-cli/src/questions.ts
+++ 003-projeto-cli/src/questions.ts
@@ -1,4 +1,6 @@
 import { EChoicesBoilerPlate } from 'enum/choices-boilerplate.enum';
+import fs from 'node:fs';
+import path from 'path';
 
 export const questions = [
   {
@@ -9,15 +11,32 @@ export const questions = [
   },
   {
     type: 'input',
-    name: 'techName',
+    name: 'folderName',
     message: 'Qual nome devo dar para a pasta do Projeto?',
-    validate(folderName: string){
-        console.log(folderName);
-        // FolderName - nao pode ser null
-        // nao pode ter caracteres especiais
-        //nao pode existir o mesmo nome do folderName
-        //nao pode deixar com o mesmo nome da repo do git
-        return true;
+    validate(folderName: string) {
+      console.log(folderName);
+      // FolderName - nao pode ser null
+      if (!folderName) return 'Insira um valor para o nome!';
+
+      // nao pode ter caracteres especiais
+      if (/[^\w\s-]/.test(folderName)) return 'Não use caracteres especiais!';
+
+      //nao pode deixar com o mesmo nome da repo do git
+      if (
+        folderName === 'boilerpalte-typescript-nodejs' ||
+        folderName === 'boilerplate-scss'
+      )
+        return 'Nao e possivel criar a pasta com este nome!';
+
+      //nao pode existir o mesmo nome do folderName
+
+      try {
+        const dir = path.resolve(folderName);
+        fs.accessSync(dir, fs.constants.R_OK);
+        return 'Ja existe uma pasta com este nome!';
+        console.log(dir);
+      } catch (error) {}
+      return true;
     },
   },
 ];
